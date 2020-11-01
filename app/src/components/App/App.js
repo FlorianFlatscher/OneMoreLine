d9e0162270a57bf891564be0d5cf847084f7913b
greenappleFF <florian.flatscher@gmail.com>
2020-11-01 20:27:28 +0100

    Setup basic page

---
+++ app/src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>One More Line</h1>
+    </div>
+  );
+}
+
+export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import * as firebase from "firebase";
-import * as dotenv from "dotenv";
+import firebase from 'firebase/app'
 
-dotenv.config()
-
-const config = {
+//Initialize firebase with .env file
+const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-};
+    appId: process.env.REACT_APP_APP_ID
+});
 
-firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -30,3 +28,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default app;
